ce3bc1710f4c8b241026bd2dd58390456cabfc3c
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-18 13:26:34 +0530

    added content

--- docker.tf
+++ docker.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "docker" {
-  ami           = local.ami_id
+  ami = local.ami_id
   #instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
-  instance_type = "t3.medium"
+  instance_type          = "t3.medium"
   # need more for terraform
   root_block_device {
     volume_size = 50
@@ -11,34 +11,34 @@ resource "aws_instance" "docker" {
   user_data = file("docker.sh")
   #iam_instance_profile = "TerraformAdmin"
   tags = {
-     Name = "${var.project}-${var.environment}-docker"
+    Name = "${var.project}-${var.environment}-docker"
   }
 }
 
 resource "aws_security_group" "allow_all_docker" {
-    name        = "allow_all_docker"
-    description = "allow all traffic"
+  name        = "allow_all_docker"
+  description = "allow all traffic"
 
-    ingress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    lifecycle {
-      create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 
-    tags = {
-        Name = "allow-all-docker"
-    }
+  tags = {
+    Name = "allow-all-docker"
+  }
 }
